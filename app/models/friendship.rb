da1131ec730b6349ece43e60193895b0e795b796
Manuel Puyol <manuelpuyol@gmail.com>
2019-10-15 20:26:17 -0500

    add annotate

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,3 +2,20 @@ class Friendship < ApplicationRecord
   belongs_to :requester
   belongs_to :receiver
 end
+
+# == Schema Information
+#
+# Table name: friendships
+#
+#  id           :integer          not null, primary key
+#  state        :string
+#  requester_id :integer          not null
+#  receiver_id  :integer          not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+# Indexes
+#
+#  index_friendships_on_receiver_id   (receiver_id)
+#  index_friendships_on_requester_id  (requester_id)
+#
