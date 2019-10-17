5dc1c371e0937b3bc1462731345de330d1d2be91
Manuel Puyol <manuelpuyol@gmail.com>
2019-10-17 17:18:27 -0500

    first setup react

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root 'app#index'
+
   mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql' if Rails.env.development?
   post '/graphql', to: 'graphql#execute'
   devise_for :users
