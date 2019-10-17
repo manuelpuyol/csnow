a4c5f154b1442ae135bc24e38707c2494363f327
Manuel Puyol <manuelpuyol@gmail.com>
2019-10-17 17:25:56 -0500

    remove node_modules, add eslintrc, prettierrc, start apollo

---
+++ app/javascript/utils/apollo.js
@@ -0,0 +1,49 @@
+// app/javascript/utils/apollo.js
+
+// client
+import { ApolloClient } from 'apollo-client';
+// cache
+import { InMemoryCache } from 'apollo-cache-inmemory';
+// links
+import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
+import { ApolloLink, Observable } from 'apollo-link';
+
+export const createCache = () => {
+  const cache = new InMemoryCache();
+  if (process.env.NODE_ENV === 'development') {
+    window.secretVariableToStoreCache = cache;
+  }
+  return cache;
+};
+
+const getToken = () =>
+  document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+const token = getToken();
+const setTokenForOperation = async operation =>
+  operation.setContext({
+    headers: {
+      'X-CSRF-Token': token,
+    },
+  });
+// link with token
+const createLinkWithToken = () =>
+  new ApolloLink(
+    (operation, forward) =>
+      new Observable(observer => {
+        let handle;
+        Promise.resolve(operation)
+          .then(setTokenForOperation)
+          .then(() => {
+            handle = forward(operation).subscribe({
+              next: observer.next.bind(observer),
+              error: observer.error.bind(observer),
+              complete: observer.complete.bind(observer),
+            });
+          })
+          .catch(observer.error.bind(observer));
+        return () => {
+          if (handle) handle.unsubscribe();
+        };
+      })
+  );
