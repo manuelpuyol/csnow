63ef1b435f97ee4f46a7f0c9cb30b0c5161c65c0
Manuel Puyol <manuelpuyol@gmail.com>
2019-10-24 18:27:47 -0500

    add tournament loading state

--- app/javascript/components/core/TournamentList/TournamentListLoading.tsx
+++ app/javascript/components/core/TournamentList/TournamentListLoading.tsx
@@ -1,13 +1,10 @@
 import React, { Fragment } from 'react';
-import { Skeleton } from 'antd';
-import Card from '@csnow/components/ui/Card/Card';
+import TournamentLoading from '@csnow/components/core/Tournament/TournamentLoading';
 
 const TournamentListLoading: React.FC = () => (
   <Fragment>
     {[1, 2, 3, 4, 5].map(i => (
-      <Card key={i}>
-        <Skeleton loading active />
-      </Card>
+      <TournamentLoading key={i} />
     ))}
   </Fragment>
 );
