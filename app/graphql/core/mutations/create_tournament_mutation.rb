85d8d066bcc7c3ab79ddf7103522b36eeb711530
Manuel Puyol <manuelpuyol@gmail.com>
2019-12-06 16:28:10 -0600

    create mutation

---
+++ app/graphql/core/mutations/create_tournament_mutation.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Core
+  module Mutations
+    class CreateTournamentMutation < ::BaseMutation
+      argument :name, String, required: true
+      argument :location, String, required: false
+      argument :start_at, Core::Types::TimeType, required: true
+      argument :end_at, Core::Types::TimeType, required: false
+
+      field :tournament, Core::Types::TournamentType, null: false
+
+      def resolve(attrs)
+        tournament = Tournament.create(attrs.merge(hltv_id: 0), includes: %i[tournament upper_roster lower_roster winner])
+
+        { tournament: tournament }
+      end
+    end
+  end
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -5,5 +5,6 @@ module Types
     field :delete_tournament, mutation: Core::Mutations::DeleteTournamentMutation
     field :update_tournament, mutation: Core::Mutations::UpdateTournamentMutation
     field :create_match, mutation: Core::Mutations::CreateMatchMutation
+    field :create_tournament, mutation: Core::Mutations::CreateTournamentMutation
   end
 end
